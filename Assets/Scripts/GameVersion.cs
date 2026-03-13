f6cc8775cff83d392479f36fbb75165b3396a9b5
Óscar Morgado <oscarmorgadocarrascocontacto@gmail.com>
2026-03-13 14:34:23 +0100

    Añadir GameVersion a los settings del menú principal

---
+++ Assets/Scripts/GameVersion.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;       
+
+public class GameVersionLabel : MonoBehaviour
+{
+    [SerializeField] private Text uiText;
+
+    private void Awake()
+    {
+        if (uiText == null)
+            uiText = GetComponent<Text>();        
+
+        uiText.text = "Game version: " + Application.version;
+    }
+}
